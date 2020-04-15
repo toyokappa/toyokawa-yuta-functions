4395dce833b19a823abbc3acb88fa0771e1084fe
Yuta Toyokawa <kppg42@gmail.com>
2020-04-15 22:22:31 +0900

    メール送信の実装

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,32 @@
 const functions = require('firebase-functions');
+const mailgun = require('mailgun-js')
+const apiKey = functions.config().mail.api_key
+const domain = functions.config().mail.domain
+const mailTo = functions.config().mail.to
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const mg = mailgun({ apiKey, domain })
+
+const mailTemplate = (data) => {
+  return `以下の内容でホームページよりお問い合わせがありました。
+【お名前】 ${data.name}
+【連絡先】 ${data.email}
+【内容】
+ ${data.message} 
+  `
+}
+
+exports.sendMail = functions.https.onCall(async (data, _context) => {
+  const mail = {
+    from: 'postmaster@sandbox775e113f7c924197aaeeb4b848707749.mailgun.org',
+    to: mailTo,
+    subject: '【TOYOKAWA】お問い合わせがありました',
+    text: mailTemplate(data)
+  }
+
+  try {
+    await mg.messages().send(mail)
+  } catch (e) {
+    console.error(`send faild: ${e}`)
+    throw new functions.https.HttpsError('internal', `send faild: ${e}`)
+  }
+})
